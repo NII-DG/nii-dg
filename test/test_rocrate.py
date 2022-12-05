7583ffecfb52b5101a8075de1730ccd7929a2369
Kaho <82209935+kahopedia@users.noreply.github.com>
2022-12-05 06:29:41 +0000

    fixed tyopo

--- test/test_rocrate.py
+++ test/test_rocrate.py
@@ -19,7 +19,7 @@ def test_add_entity(crate):
         "@type":"Type",
         "name":"test"
     }
-    # if and entity already exists
+    # if an entity already exists
     assert crate.add_entity("id", "Type", {"description":"update entity"}) == {"@id":"id"}
     assert len(crate.entities) == 3
     assert crate.entities[2].get_jsonld() == {
