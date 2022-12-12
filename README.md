ff46dbc8870a7cbc8ec103731784c9bbe90c8608
Kaho <82209935+kahopedia@users.noreply.github.com>
2022-12-12 08:58:11 +0000

    applied format

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # 検証
+
 TBU
 
-NIIガバナンス用スキーマに沿ったRO-Crateを用意し、検証ルールを指定する?
+NII ガバナンス用スキーマに沿った RO-Crate を用意し、検証ルールを指定する?
 
-# RO-Crate生成ライブラリ
-NIIガバナンス用スキーマに沿ったRO-CrateをJSONから生成するためのライブラリ
+# RO-Crate 生成ライブラリ
+
+NII ガバナンス用スキーマに沿った RO-Crate を JSON から生成するためのライブラリ
+
 ## 利用方法
+
 ### 入力ファイルの準備
-メタデータ情報をもつJSONファイル(もしくはYAML)を作成する。[サンプル](https://github.com/ascade/nii-dg/blob/f0f76213d5365ab5ed43902028060a335b8edb34/test/common_sample.json)
+
+メタデータ情報をもつ JSON ファイル(もしくは YAML)を作成する。[サンプル](https://github.com/ascade/nii-dg/blob/f0f76213d5365ab5ed43902028060a335b8edb34/test/common_sample.json)
 
 内容には研究メタデータの他、DMP(データマネジメントプラン)の情報を含む。
+
 ```
 ・DMPの形式
 ・プロジェクト名
@@ -21,10 +27,13 @@ NIIガバナンス用スキーマに沿ったRO-CrateをJSONから生成する�
 ・DMPの記載内容
 等
 ```
+
 [詳細ドキュメント](https://github.com/ascade/nii-dg/blob/9d56cba94da139bf5ec23d5432d48dbafc9d6097/test/README.md)
 
-### RO-Crate生成 
-JSONファイルに対象データ情報が含まれる場合、それを引数に指定する。現在のディレクトリに`ro-crate-metadata.json`が生成される。
+### RO-Crate 生成
+
+JSON ファイルに対象データ情報が含まれる場合、それを引数に指定する。現在のディレクトリに`ro-crate-metadata.json`が生成される。
+
 ```python
 import nii_dg
 
@@ -32,6 +41,7 @@ nii_dg.generate_rocrate('path/to/input.json')
 ```
 
 対象データ情報をローカルから読み込む場合、そのディレクトリパスを第二引数に置く。同様に`ro-crate-metadata.json`が生成される。
+
 ```python
 import nii_dg
 
--- nii_dg/generate.py
+++ nii_dg/generate.py
@@ -1,12 +1,15 @@
 import json
 import os
+
 import jsonschema
-from nii_dg.model.rocrate import NIIROCrate
+
 from nii_dg import const
+from nii_dg.model.rocrate import NIIROCrate
 
 
 class ValidationError(Exception):
     pass
+
 
 def read_dmp(path):
     '''
@@ -21,7 +24,7 @@ def validate_with_schema(dict_):
     '''
     JSON-Schemaで入力を検証
     '''
-    with open(os.path.dirname(__file__) +'/schema.json', encoding='utf-8') as schemafile:
+    with open(os.path.dirname(__file__) + '/schema.json', encoding='utf-8') as schemafile:
         schema = json.load(schemafile)
 
     error_messages = []
@@ -31,6 +34,7 @@ def validate_with_schema(dict_):
 
     if len(error_messages) > 0:
         raise ValidationError('\n'.join(error_messages))
+
 
 def check_dmp_format(dict_):
     '''
@@ -54,7 +58,7 @@ def generate_crate_instance(dict_):
     return NIIROCrate(dict_)
 
 
-def add_entities_to_crate(crate:NIIROCrate) -> None:
+def add_entities_to_crate(crate: NIIROCrate) -> None:
     '''
     入力JSONのkeyに対して対応するメソッドを実行しコンテキストエンティティを追加
     '''
@@ -62,19 +66,19 @@ def add_entities_to_crate(crate:NIIROCrate) -> None:
 
     key_func = {
         "projectName": crate.set_project_name,
-        "fundingAgency":crate.set_funder,
-        "repository":crate.set_repo,
-        "researchField":crate.set_field,
-        "e-RadProjectId":crate.set_erad,
-        "license":crate.set_license,
-        "creator":crate.set_creators
-        }
+        "fundingAgency": crate.set_funder,
+        "repository": crate.set_repo,
+        "researchField": crate.set_field,
+        "e-RadProjectId": crate.set_erad,
+        "license": crate.set_license,
+        "creator": crate.set_creators
+    }
     funcs = {k: v for k, v in key_func.items() if k in crate.dmp}
     for k, v in funcs.items():
         if crate.dmp.get(k) is not None:
             v(crate.dmp.get(k))
-    
+
     if crate.dmp_format == 'common_metadata':
         crate.set_dmp_common()
     else:
-        pass #tbd
+        pass  # tbd
