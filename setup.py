3b96cabeaba6149c79a219b1326fd37e380e1914
Kaho <82209935+kahopedia@users.noreply.github.com>
2022-12-14 08:32:04 +0000

    added required package

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setup(
     python_requires=">=3.7",
     platforms="any",
     packages=["nii_dg"],
+    install_requires=[
+        "yaml",
+    ],
     tests_require=[
         "flake8",
         "isort",
