390831fdc8e32293642e81cbdcb370f888c1f84c
Kaho <82209935+kahopedia@users.noreply.github.com>
2023-02-13 10:13:02 +0000

    Introduce furures to api.py

--- nii_dg/api.py
+++ nii_dg/api.py
@@ -2,120 +2,134 @@
 # coding: utf-8
 
 import threading
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict, List, Optional
 from uuid import uuid4
 
-from flask import Flask, Response, abort, jsonify, request
+from flask import Blueprint, Flask, Response, abort, jsonify, request
 
 from nii_dg.error import GovernanceError
 from nii_dg.ro_crate import ROCrate
 
-app = Flask(__name__)
+GET_STATUS_CODE = 200
+POST_STATUS_CODE = 200
+JOB_STATUS = [
+    "UNKNOWN",
+    "QUEUED",
+    "RUNNING",
+    "COMPLETE",
+    "FAILED",
+    "CANCELING",
+    "CANCELED",
+]
 
 
-def get_file_name(request_id: str) -> Path:
-    log_file: Path = Path.cwd().joinpath('log_file', request_id)
-    return log_file
+# --- state ---
 
 
-def read_log_file(request_id: str) -> str:
-    try:
-        with get_file_name(request_id).open(mode="r", encoding="utf-8") as f:
-            return f.read()
-    except FileNotFoundError:
-        abort(400, f"Request ID {request_id} is not existing.")  # type:ignore
+executor = ThreadPoolExecutor(max_workers=3)
+job_map: dict = {}
+request_map: dict = {}
 
 
-def write_to_log_file(request_id: str, content: Any) -> None:
-    get_file_name(request_id).mkdir(parents=True, exist_ok=True)
-    with get_file_name(request_id).open(mode="w", encoding="utf-8") as f:
-        f.write(str(content))
+# --- controller ---
+
+app_bp = Blueprint("app", __name__)
 
 
-class CrateValidation(threading.Thread):
-    def __init__(self, request_id: str) -> None:
-        super().__init__()
-        self.request_id = request_id
-        self.stop_event = threading.Event()
-
-    def stop(self):
-        self.stop_event.set()
-
-    def run(self):
-        pass
-        # try:
-        #     for _ in range(1000):
-        #         print(f'{datetime.now():%H:%M:%S}')
-        #         sleep(1)
-
-        #         # 定期的にフラグを確認して停止させる
-        #         if self.stop_event.is_set():
-        #             break
-        # finally:
-        #     print('時間のかかる処理が終わりました\n')
-
-
-@app.errorhandler(404)
+@app_bp.errorhandler(400)
 def invalid_crate(e: Exception) -> Response:
-    return jsonify(message=str(e)), 404
-
-# Governance function
+    return jsonify(message=str(e)), 400
 
 
-@app.route('/validate', methods=['POST'])
-def post_crates() -> Response:
-    crate_file = request.files.get("ro-crate")
-    entity_ids = request.args.get("entityIds")
-
-    if crate_file is None:
-        # TODO
-        raise Exception("need ro-crate-metadata.json as a request body")
-    if crate_file.mimetype == "application/json":
-        crate_str = crate_file.read().decode()
-    else:
-        # TODO
-        raise Exception("ro-crate MUST be a json file")
-
+@app_bp.route('/validate', methods=['POST'])
+def request_validation() -> Response:
     request_id = str(uuid4())
+    request_body = request.json or {}
+    entity_ids = request.args.get("entityIds", None)
+
+    with current_app.app_context():  # noqa:F821
+        request_map[request_id] = request_body
+
+    if request_body == {}:
+        # TODO
+        raise Exception("To data governance, ro-crate-metadata.json is required as a request body.")
+
     # TODO: id記録してvalidate()実行
-    write_to_log_file(request_id, "QUEUED")
-    t = CrateValidation(request_id)
-    t.start()
+    job = executor.submit(validate, request_body, entity_ids)
+    job_map[request_id] = job
+
     return jsonify({"request_id": request_id}), 200
 
-    # # クエリパラメータ
-    # # query = request.form
-    # # hoge = query.get("id")
-    # crate = ROCrate(from_jsonld=json.loads(crate_str))
 
-    # try:
-    #     crate.validate()
-    #     return Response(response=jsonify({"status": "SUCCEEDED"}), status=200)
-    # except GovernanceError as gov_error:
-    #     return Response(response=jsonify({"status": "FAILED",
-    #                                       "failures": json.loads(str(gov_error))}), status=200)
-    # except Exception:
-    #     # TODO: 例外クラスを限定する
-    #     abort(400, description="Invalid Crate")
+@app_bp.route('/<request_id:str>', methods=['GET'])
+def get_results() -> None:
+    if request_id not in job_map:
+        abort(400, "request_id not found")
+    job = job_map[request_id]
+    request = request_map[request_id]
 
-
-@app.route('/<requestId:str>', methods=['GET'])
-def get_result() -> None:
-    # TODO
+    # TODO: add status and return result
     pass
 
 
-@app.route('/<requestId:str>/cancel', methods=['POST'])
-def cancel_process() -> None:
+@app_bp.route('/<request_id:str>/cancel', methods=['POST'])
+def cancel_validation() -> None:
+    if request_id not in job_map:
+        abort(400, "request_id not found")
+    job = job_map[request_id]
+    try_cancel = job.cancel()
+    if not try_cancel:
+        abort(400, "Failed to cancel")
+    response: Response = jsonify({"request_id": request_id})
+    response.status_code = POST_STATUS_CODE
+
     # TODO
-    pass
+
+    return response
 
 
-@app.route('/healthcheck', methods=['GET'])
+@app_bp.route('/healthcheck', methods=['GET'])
 def check_health() -> Response:
-    return Response(status=200)
+    return Response(jsonify({"message": "OK"}), status=200)
+
+# --- job ---
+
+
+def validate(request: Dict[str, Any], entity_ids: Optional[List[str]]) -> List[Dict[str, Any]]:
+    crate = ROCrate(request)
+    if entity_ids:
+        # TODO
+        pass
+    else:
+        try:
+            crate.validate()
+            # TODO
+            pass
+        except GovernanceError as e:
+            # TODO
+            return str(e.entity_errors)
+
+
+# --- app ---
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(app_bp)
+
+    # for debug
+    app.config["FLASK_ENV"] = "development"
+    app.config["DEBUG"] = True
+    app.config["TESTING"] = True
+
+    return app
+
+
+def main() -> None:
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    main()
