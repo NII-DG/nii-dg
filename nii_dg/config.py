3bf10f02734862c2b6e7161b9e4cc65407d18e38
Kaho <82209935+kahopedia@users.noreply.github.com>
2023-02-22 06:42:10 +0000

    Separate file

---
+++ nii_dg/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+def github_repo() -> str:
+    # TODO use environment variable, git command, or const value (where?)
+    return "NII-DG/nii_dg"
+
+
+def github_branch() -> str:
+    # TODO use environment variable, git command, or const value (where?)
+    return "main"
