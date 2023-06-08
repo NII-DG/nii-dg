211aa945b9f522c7e700fdd07a5298a5c5f29c13
suecharo <suehiro619@gmail.com>
2023-06-08 20:42:17 +0900

    Fix rewrite_version script

--- rewrite_version.sh
+++ rewrite_version.sh
@@ -11,21 +11,28 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
+PREV_VERSION=$(python3 ./nii_dg/module_info.py)
 NEW_VERSION=$1
 
-# Define the locations and current version patterns to replace
-declare -A LOCATIONS=(
-  ["./nii_dg/module_info.py"]="GH_REF: str = \"[0-9]*\.[0-9]*\.[0-9]*\""
-  ["./Dockerfile"]="LABEL org.opencontainers.image.version=\"[0-9]*\.[0-9]*\.[0-9]*\""
-  ["./compose.yml"]="image: ghcr.io\/nii-dg\/nii-dg:[0-9]*\.[0-9]*\.[0-9]*"
-  ["./compose.api.yml"]="image: ghcr.io\/nii-dg\/nii-dg:[0-9]*\.[0-9]*\.[0-9]*"
-)
+read -p "Rewrite version from ${PREV_VERSION} to ${NEW_VERSION}? (y/n) :" YN
 
-# Iterate over the locations and update the version
-for FILE in "${!LOCATIONS[@]}"; do
-  PATTERN=${LOCATIONS[$FILE]}
-  echo "Updating version in $FILE ..."
-  sed -i -r "s/${PATTERN}/${PATTERN%%[0-9]*\.[0-9]*\.[0-9]*\"}${NEW_VERSION}\"/g" $FILE
-done
+if [[ "${YN}" != "y" ]]; then
+  echo "Abort."
+  exit 1
+fi
+
+echo "Rewriting version from ${PREV_VERSION} to ${NEW_VERSION} ..."
+
+echo "Rewriting nii_dg/module_info.py ..."
+sed -i "s/GH_REF: str = \"${PREV_VERSION}\"/GH_REF: str = \"${NEW_VERSION}\"/g" ./nii_dg/module_info.py
+
+echo "Rewriting Dockerfile ..."
+sed -i "s/LABEL org.opencontainers.image.version=\"${PREV_VERSION}\"/LABEL org.opencontainers.image.version=\"${NEW_VERSION}\"/g" ./Dockerfile
+
+echo "Rewriting compose.yml ..."
+sed -i "s/image: ghcr.io\/NII-DG\/nii-dg:${PREV_VERSION}/image: ghcr.io\/NII-DG\/nii-dg:${NEW_VERSION}/g" ./compose.yml
+
+echo "Rewriting compose.api.yml ..."
+sed -i "s/image: ghcr.io\/NII-DG\/nii-dg:${PREV_VERSION}/image: ghcr.io\/NII-DG\/nii-dg:${NEW_VERSION}/g" ./compose.api.yml
 
 echo "Version update complete."
