066f5b1aa953a6b7dab383e240e51747c521d852
suecharo <suehiro619@gmail.com>
2023-06-08 15:01:41 +0900

    Refactoring script

--- rewrite_version.sh
+++ rewrite_version.sh
@@ -1,54 +1,33 @@
 #!/usr/bin/env bash
 
-# rewrite version in:
-#
-# - nii_dg/module_info.py
-#    - `GH_REF: str = "0.1.0"`
-# - docs/conf.py
-#    - `version = '0.1.0'`
-#    - `release = '0.1.0'`
-# - Dockerfile
-#    - `LABEL org.opencontainers.image.version="0.1.0"`
-# - compose.yml
-#    - `image: ghcr.io/nii-dg/nii-dg:0.1.0`
-# - compose.api.yml
-#    - `image: ghcr.io/nii-dg/nii-dg:0.1.0`
+# This script updates the version number of the NII-DG library across multiple files.
+# Usage: ./<scriptname> <new_version>
 
-set -euxo pipefail
+set -euo pipefail
 
+# Ensure the new version number is provided as an argument
 if [[ $# -lt 1 ]]; then
   echo "Usage: $0 <new_version>"
   exit 1
 fi
 
-PREV_VERSION=$(python3 ./nii_dg/module_info.py)
 NEW_VERSION=$1
 
-read -p "Rewrite version from ${PREV_VERSION} to ${NEW_VERSION}? (y/n) :" YN
+# Define the locations and current version patterns to replace
+declare -A LOCATIONS=(
+  ["./nii_dg/module_info.py"]="GH_REF: str = \"[0-9]*\.[0-9]*\.[0-9]*\""
+  ["./docs/conf.py"]="version = '[0-9]*\.[0-9]*\.[0-9]*'"
+  ["./docs/conf.py"]="release = '[0-9]*\.[0-9]*\.[0-9]*'"
+  ["./Dockerfile"]="LABEL org.opencontainers.image.version=\"[0-9]*\.[0-9]*\.[0-9]*\""
+  ["./compose.yml"]="image: ghcr.io\/nii-dg\/nii-dg:[0-9]*\.[0-9]*\.[0-9]*"
+  ["./compose.api.yml"]="image: ghcr.io\/nii-dg\/nii-dg:[0-9]*\.[0-9]*\.[0-9]*"
+)
 
-if [[ "${YN}" != "y" ]]; then
-  echo "Abort."
-  exit 1
-fi
+# Iterate over the locations and update the version
+for FILE in "${!LOCATIONS[@]}"; do
+  PATTERN=${LOCATIONS[$FILE]}
+  echo "Updating version in $FILE ..."
+  sed -i -r "s/${PATTERN}/${PATTERN%%[0-9]*\.[0-9]*\.[0-9]*\"}${NEW_VERSION}\"/g" $FILE
+done
 
-echo "Rewriting version from ${PREV_VERSION} to ${NEW_VERSION} ..."
-
-echo "Rewriting nii_dg/module_info.py ..."
-sed -i "s/GH_REF: str = \"${PREV_VERSION}\"/GH_REF: str = \"${NEW_VERSION}\"/g" ./nii_dg/module_info.py
-
-echo "Rewriting docs/conf.py ..."
-sed -i "s/version = '${PREV_VERSION}'/version = '${NEW_VERSION}'/g" ./docs/conf.py
-sed -i "s/release = '${PREV_VERSION}'/release = '${NEW_VERSION}'/g" ./docs/conf.py
-
-echo "Rewriting Dockerfile ..."
-sed -i "s/LABEL org.opencontainers.image.version=\"${PREV_VERSION}\"/LABEL org.opencontainers.image.version=\"${NEW_VERSION}\"/g" ./Dockerfile
-
-echo "Rewriting compose.yml ..."
-sed -i "s/image: ghcr.io\/nii-dg\/nii-dg:${PREV_VERSION}/image: ghcr.io\/nii-dg\/nii-dg:${NEW_VERSION}/g" ./compose.yml
-
-echo "Rewriting compose.api.yml ..."
-sed -i "s/image: ghcr.io\/nii-dg\/nii-dg:${PREV_VERSION}/image: ghcr.io\/nii-dg\/nii-dg:${NEW_VERSION}/g" ./compose.api.yml
-
-echo "Done."
-
-exit 0
+echo "Version update complete."
